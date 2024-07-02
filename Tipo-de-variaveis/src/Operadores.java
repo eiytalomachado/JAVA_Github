2fcfc6ce58559132ec8ebe01d36074f310113265
Ytalo machado <ytalomachadosantos@gmail.com>
2024-07-02 20:31:03 -0300

    feito

--- Tipo-de-variaveis/src/Operadores.java
+++ Tipo-de-variaveis/src/Operadores.java
@@ -1,6 +1,22 @@
 public class Operadores {
     public static void main(String[] args) {
-        String nomeCompleto = "LINGAUGEM" + "JAVA";
-        System.out.println(nomeCompleto);
+      int numeroUm = 1;
+      int numeroDois = 2;
+      
+      boolean simNao = numeroUm == numeroDois;
+
+      System.out.println("NúmeroUm é igual ao NúmeroDois? " + simNao);
+
+      simNao = numeroUm != numeroDois;
+
+      System.out.println("NúmeroUm é diferente ao NúmeroDois? " + simNao);
+
+      simNao = numeroUm > numeroDois;
+
+      System.out.println("NúmeroUm é maior ao NúmeroDois? " + simNao);
+
+      simNao = numeroUm < numeroDois;
+
+      System.out.println("NúmeroUm é maior ao NúmeroDois? " + simNao);
     }
 }
