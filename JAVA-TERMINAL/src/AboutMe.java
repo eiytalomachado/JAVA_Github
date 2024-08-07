90f7e1ab5d4e47de600556da47e51870d3306da3
Ytalo machado <ytalomachadosantos@gmail.com>
2024-08-07 08:02:53 -0300

    atualizado

---
+++ JAVA-TERMINAL/src/AboutMe.java
@@ -0,0 +1,19 @@
+public class AboutMe {
+    public static void main(String[] args) throws Exception {
+        
+        String nome = args [0];
+        String sobrenome = args [1];
+        int idade = Integer.valueOf(args [2]);
+        double altura = Double.valueOf(args [3]);
+        Double largura = Double.valueOf (args [4]);
+        
+        System.out.println( "Olá, me chamo " + nome + " " + sobrenome);
+        System.out.println( "Tenho " + idade + " anos" );
+        System.out.println( "Minha altura é " + altura + " cm ");
+        System.out.println("e " + largura + " de largura!");
+
+
+
+
+    }
+}
