0dee3fab579c8765d10e2b417480e82a9977ed7b
Ytalo machado <ytalomachadosantos@gmail.com>
2024-08-08 07:34:22 -0300

    novo

--- JAVA-TERMINAL/src/AboutMe.java
+++ JAVA-TERMINAL/src/AboutMe.java
@@ -1,19 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         
-        String nome = args [0];
-        String sobrenome = args [1];
-        int idade = Integer.valueOf(args [2]);
-        double altura = Double.valueOf(args [3]);
-        Double largura = Double.valueOf (args [4]);
-        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Qual é o seu nome?");
+        String nome = scanner.next();
+
+        System.out.println("Qual é o seu sobrenome?");
+        String sobrenome = scanner.next();
+
+        System.out.println("Qual e a sua idade?");
+        int idade = scanner.nextInt();
+
+        System.out.println("Qual é a sua altura?");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Qual é a sua largura?");
+        double largura = scanner.nextDouble();
+
         System.out.println( "Olá, me chamo " + nome + " " + sobrenome);
         System.out.println( "Tenho " + idade + " anos" );
         System.out.println( "Minha altura é " + altura + " cm ");
         System.out.println("e " + largura + " de largura!");
-
-
-
-
     }
 }
